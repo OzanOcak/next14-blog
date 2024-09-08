aa0d9037629f99b41c46a9303ff49bc9b44115da
OzanOcak <ozanocak@gmail.com>
2024-09-07 23:20:58 -0400

    UI/UX of the website

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,15 @@
 export default function Home() {
-  return <main>Home</main>;
+  return (
+    <main className="text-center pt-32 px-5">
+      <h1 className="text-4xl md-5xl font-bold mb-5">Welcome to my blog</h1>
+      <p className="max-w-[750px] mx-auto">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea alias in
+        excepturi fugiat quia ipsam quisquam modi expedita autem, eaque
+        molestiae aliquid sit sed nemo consequuntur, neque officia
+        necessitatibus corrupti distinctio nulla ducimus architecto vel sunt
+        ipsum! Modi reiciendis quas repellendus eaque, esse hic explicabo,
+        adipisci magnam, pariatur id odio.
+      </p>
+    </main>
+  );
 }
