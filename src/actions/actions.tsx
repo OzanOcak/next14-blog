c31ff485126d62c476deff35a40daf7205d9ec65
OzanOcak <ozanocak@gmail.com>
2024-09-09 10:59:24 -0400

    auth

--- src/actions/actions.tsx
+++ src/actions/actions.tsx
@@ -1,6 +1,15 @@
 "use server";
 
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { redirect } from "next/navigation";
+
 export async function createPost(formData: FormData) {
+  // auth check
+  const { isAuthenticated } = getKindeServerSession();
+  if (!(await isAuthenticated())) {
+    redirect("/api/auth/login?post_login_redirect_url=/create-post");
+  }
+
   const title = formData.get("title") as string;
   const body = formData.get("body") as string;
 }
